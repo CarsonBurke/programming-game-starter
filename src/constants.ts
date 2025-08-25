b9c85060c0acc963d3c5be53ddac1ccd7b3f432a
Carson <carsonburke22@gmail.com>
2025-08-24 17:19:33 -0700

    fix pos

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 import { FoodItems } from "programming-game/items";
 
 export const FOOD = new Set<FoodItems>(["snakeMeat", "ratMeat", "chickenMeat", "edibleSlime"]);
-export const MAP_SIZE = Number.MAX_SAFE_INTEGER
+export const MAP_SIZE = 10_000;
 export const SPAWN_POS = { x: 0, y: 0 }
